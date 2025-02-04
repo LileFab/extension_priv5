585cf3123070f68eaf00bc14d8f3dd219a1c9f8d
Fabien <backspeen@gmail.com>
2025-02-04 23:09:19 +0100

    ⚰️ clean

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,3 @@
-// Définition des états possibles de la salle
 const ETATS = {
 	LIBRE: "libre",
 	PRIS: "pris",
@@ -35,21 +34,16 @@ const setState = async (newState) => {
 	});
 };
 
-// État initial de la salle (nous utiliserons le stockage Chrome plus tard)
 let etatActuel = null;
 
-// Attendre que le document soit chargé avant d'exécuter notre code
 document.addEventListener("DOMContentLoaded", async () => {
-	// Récupération des éléments de l'interface
 	const boutonStatusPprod = document.getElementById("toggle-status-pprod");
 	const indicateurStatusPprod = document.getElementById(
 		"status-indicator-pprod"
 	);
 	const textButtonPprod = document.getElementById("button-text-pprod");
 
-	// Fonction pour mettre à jour l'affichage
 	function update(etat) {
-		// Mise à jour des classes CSS
 		indicateurStatusPprod.className = `status-indicator status-${etat}`;
 		textButtonPprod.textContent = `${
 			etat === ETATS.LIBRE ? TEXTEETAT.LIBRE : TEXTEETAT.PRIS
@@ -63,16 +57,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 		}, 1000); // Vérifie toutes les seconde
 	}
 
-	// Gestion du clic sur le bouton
 	boutonStatusPprod.addEventListener("click", async () => {
-		// Changement de l'état
 		etatActuel = await recupererEtat();
 		await setState(etatActuel === ETATS.LIBRE ? "1" : "0");
-		// Mise à jour de l'interface
 		update(etatActuel);
 	});
 
-	// Initialisation de l'interface
 	demarrerPolling();
 	etatActuel = await recupererEtat();
 	update(etatActuel);
